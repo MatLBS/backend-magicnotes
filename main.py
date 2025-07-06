0b4f6c4466607a6cd796040f47e9ca30cbf7c96d
MatLBS <135004599+MatLBS@users.noreply.github.com>
2025-07-06 13:33:19 +0200

    change port

--- main.py
+++ main.py
@@ -31,4 +31,4 @@ async def upload_image(file: UploadFile = File(...)):
     return highlighted_words
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
